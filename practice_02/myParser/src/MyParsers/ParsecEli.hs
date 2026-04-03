cefe7913e19afd66f507e0ccc1a2d53337a27159
AKAbethename <crazybobov5@gmail.com>
2026-04-03 04:09:14 +0300

    Добавление функций myDigit myLower myChar

--- practice_02/myParser/src/MyParsers/ParsecEli.hs
+++ practice_02/myParser/src/MyParsers/ParsecEli.hs
@@ -4,6 +4,8 @@ import MyTypes.MyMaybe
 
 import Text.Parsec
 import Text.Parsec.String (Parser)  -- Parser для String
+import Data.Char (isLower, isUpper, digitToInt, isDigit)
+import Control.Applicative (Alternative(..), optional, ZipList(..))
 
 charA :: Parser Char
 charA = char 'A'
@@ -11,3 +13,21 @@ charA = char 'A'
 
 mySatisfy :: (Char -> Bool) -> Parser Char
 mySatisfy p = satisfy p
+
+myChar :: Char -> Parser Char
+myChar c = char c
+
+
+myLower :: Parser Char
+myLower = lower  
+
+
+myDigit :: Parser Int
+myDigit = digitToInt <$> digit
+
+
+runParser :: Parser a -> String -> MyMaybe (String, a)
+runParser p input = 
+    case parse p "" input of
+        Left _ -> MyNothing
+        Right result -> MyJust (input, result)
