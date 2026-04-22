d5cb5210c2ac1fde97c985194ef574dd57741c18
AKAbethename <crazybobov5@gmail.com>
2026-04-22 03:23:58 +0300

    practice_03-ver-2026-04-22-03-24

--- practice_03/kurt/app/Main.hs
+++ practice_03/kurt/app/Main.hs
@@ -4,28 +4,30 @@ import qualified Data.ByteString.Char8 as BC
 
 import Lib
 
-randomReplaceByte :: BC.ByteString -> IO BC.ByteString
+randomReplaceByte :: BC.ByteString -> IO (Int, BC.ByteString)
 randomReplaceByte bytes = do
     let byteLength = BC.length bytes
     location <- randomRIO (1, byteLength)
     chV <- randomRIO (0, 255)
-    return (replaceByte location chV bytes)
+    return (chV, (replaceByte location chV bytes))
 
 
-randomSortSection :: BC.ByteString -> IO BC.ByteString
+randomSortSection :: BC.ByteString -> IO (Int, BC.ByteString)
 randomSortSection bytes = do
     let sectionSize = 25
     let bytesLength = BC.length bytes
     start <- randomRIO (0, bytesLength - sectionSize)
-    return (sortSection start sectionSize bytes)
+    return (start, (sortSection start sectionSize bytes))
 
 main :: IO ()
 main = do
     args <- getArgs
     let fileName = head args
     imageFile <- BC.readFile fileName
-    glitched <- randomReplaceByte imageFile
-    let glitchedFileName = mconcat ["glitched_", fileName]  
-    BC.writeFile glitchedFileName glitched 
+    (change1, glitched1) <- randomReplaceByte imageFile
+    (change2, glitched2) <- randomSortSection glitched1
+    let strInt = intToChar change2
+    let glitchedFileName = mconcat ["glitched_", [strInt], "_", fileName ]  
+    BC.writeFile glitchedFileName glitched2 
     putStrLn "Готово!"
 
