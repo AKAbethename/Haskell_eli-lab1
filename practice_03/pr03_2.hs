27eb35408218ce579a986a6ac586451ed762aef0
AKAbethename <crazybobov5@gmail.com>
2026-04-24 05:27:40 +0300

    practice_03-ver-2026-04-24-05-27

--- practice_03/pr03_2.hs
+++ practice_03/pr03_2.hs
@@ -4,6 +4,9 @@
 
 module Pr03_2 where
 
+import qualified Control.Monad.RWS as RWS
+import Data.Maybe ( fromMaybe )
+
 {-
 
 Реализуйте поиск маршрута прохождения лабиринта с использованием монады RWS
@@ -39,10 +42,56 @@ grz = "GRZ" :: Room
 mkhch = "MKHCH" :: Room
 
 start = "START" :: Room
-finish = "FINIST" :: Room
+finish = "FINISH" :: Room
+
+
+tupik = "tupik" :: Room
 
 
 labirint = Labir start [Labir chlb [Labir ekb [Labir krd [Labir kzn [Labir smr [Labir ufa [], Labir finish [] ] ], Labir grz [Labir smr [Labir ufa [], Labir finish []], Labir mkhch [Labir prm [Labir ufa [] ] ] ] ] ] ],
                         Labir msk [Labir kgd [Labir krd [Labir kzn [Labir smr [Labir ufa [], Labir finish [] ] ], Labir grz [Labir smr [Labir ufa [], Labir finish []], Labir mkhch [Labir prm [Labir ufa [] ] ] ] ] ] ],
                         Labir spb [Labir kgd [Labir krd [Labir kzn [Labir smr [Labir ufa [], Labir finish [] ] ], Labir grz [Labir smr [Labir ufa [], Labir finish []], Labir mkhch [Labir prm [Labir ufa [] ] ] ] ] ], 
                                     Labir uud [Labir rst [Labir grz [Labir smr [Labir ufa [], Labir finish []], Labir mkhch [Labir prm [Labir ufa [] ] ] ], Labir mkhch [Labir prm [Labir ufa [] ] ] ] ] ] ]
+
+
+data StatE = Found | Finding deriving Show
+
+
+getCurRoom :: Labir -> Room
+getCurRoom (Labir room list) = room
+
+
+getNextRooms :: Labir -> [Room]
+getNextRooms (Labir room (r:rest)) = (getCurRoom r) : getNextRooms (Labir room rest)
+getNextRooms (Labir room []) = []
+
+
+goNextFirstRoom :: Maybe Labir -> Maybe Labir
+goNextFirstRoom (Just (Labir room list)) = case length list of
+                                        0 -> Nothing
+                                        _ -> Just (head list)
+
+
+toNextFirstRoom :: Labir -> Labir
+toNextFirstRoom (Labir room list) = fromMaybe (Labir tupik []) (goNextFirstRoom (Just (Labir room list)))
+
+
+{- findRoute :: RWS.RWS (Labir room list) [Room] StatE [Room]
+findRoute = -}
+
+
+
+
+
+rwsExample :: RWS.RWS Int [String] Int Int
+rwsExample =
+    RWS.ask >>= \coefficient ->                                 -- коэффициент из Reader
+    RWS.get >>= \counter ->                                     -- текущее состояние (счетчик)
+    RWS.put (counter + 1) >>                                    -- изменение состояния
+    RWS.tell ["Increas counter to " ++ show (counter + 1)] >>   -- логирование действия
+    return (coefficient * counter)                              -- результат (коэффициент * счетчик)
+
+(resultRWS, logsRWS, finalStateRWS) = RWS.runRWS rwsExample 5 1
+
+
+
