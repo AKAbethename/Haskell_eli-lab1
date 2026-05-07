1695cd74bfba79b9d4cb5b00780030f1c544df9b
AKAbethename <crazybobov5@gmail.com>
2026-05-08 02:47:06 +0300

    practice_04-ver-2026-05-08-02-47

--- practice_04/transformer/src/Lib.hs
+++ practice_04/transformer/src/Lib.hs
@@ -34,8 +34,6 @@ start = "START" :: Room
 finish = "FINISH" :: Room
 
 gameOver = "GAMEOVER" :: Room
-
-
 tupik = "tupik" :: Room
 
 
@@ -49,7 +47,6 @@ data StatE = Found | Finding | Tupik | Lose deriving Show
 
 
 type Log    = [String]
-type Path   = [Room]
 
 getCurRoom :: Labir -> Room
 getCurRoom (Labir room list) = room
@@ -59,91 +56,15 @@ getNextRooms (Labir room (r:rest)) = (getCurRoom r) : getNextRooms (Labir room r
 getNextRooms (Labir room []) = []
 
 
-children :: Labir -> [Labir]
-children (Labir room ch) = ch
-
-
-explore :: Labir -> RWS.RWS Labir Log Path Bool
-explore node =
-  let name = getCurRoom node
-  in
-    if name == finish
-    then RWS.modify (name :) >> RWS.tell [name] >> return True
-    else exploreChildren (children node) >>= \found ->
-           if found
-           then RWS.modify (name :) >> RWS.tell [name] >> return True
-           else return False
-
-exploreChildren :: [Labir] -> RWS.RWS Labir Log Path Bool
-exploreChildren [] = return False
-exploreChildren (c:cs) =
-  explore c >>= \found ->
-  if found then return True
-  else exploreChildren cs
-
-
-
-{- choice :: Labir -> Either Int Room -> WriterT Log (ReaderT Labir Identity) Bool
-choice room_s (Left k) = do
-                            tell "Route is ended"
-                            return (True)
-choice room_s (Right room_to) = do -}
-
-
-
-choice :: [Room] -> IO Room
-choice cur_list = do
-                    putStrLn "Input the next room"
-                    city <- getLine
-                    return city
-
-
-
 
  ----------------------------------------------------- НОВЫЕ ФУНКЦИИ -----------------------------------
 
-
-{- getNextRooms :: Labir -> [Room]
-getNextRooms (Labir room (r:rest)) = (getCurRoom r) : getNextRooms (Labir room rest)
-getNextRooms (Labir room []) = [] -}
-
-
-getNextRooms' :: Either String Labir -> [Room]
-getNextRooms' (Left str) = []
-getNextRooms' (Right (Labir room [])) = []
-getNextRooms' (Right (Labir room (r:rest))) = ((getCurRoom r) : getNextRooms (Labir room rest))
-
-{- toNextRoom :: Labir -> Room -> Either String Labir
-toNextRoom (Labir room_s []) room_to = Left "Error" 
-
-toNextRoom (Labir room_s (l:ls)) room_to = if getCurRoom l == room_to
-                                            then Right l 
-                                            else toNextRoom (Labir room_s ls) room_to -}
 
 toNextRoom :: Labir -> Room -> Labir
 toNextRoom (Labir room_s []) room_to = Labir tupik []
 toNextRoom (Labir room_s (l:ls)) room_to = if getCurRoom l == room_to then l else toNextRoom (Labir room_s ls) room_to
 -- здесь точно следующая комната есть, ошибки нету
 
-
-toNextRoom' :: Either String Labir -> Room -> Either String Labir
-toNextRoom' (Left "Error") _ = Left "Error"
-toNextRoom' (Right (Labir room_s (l:ls))) room_to = if getCurRoom l == room_to
-                                                 then Right l 
-                                      --          else (toNextRoom' (Right (Labir room_s ls)) room_to)
-                                                 else if length ls == 0 
-                                                      then (toNextRoom' (Left "Error") room_to)
-                                                      else (toNextRoom' (Right (Labir room_s ls)) room_to) 
-
-
-
-choice_with_monad :: Room -> WriterT Log (ReaderT [Room] Identity) Bool
-choice_with_monad our_choice = do
-                        cur_list <- lift ask
-                        let sign = elem our_choice cur_list 
-                        if our_choice == gameOver then tell ["game over"] >> return True
-                            else if (sign == False) then tell ["Error_eli"] >> return False else tell [our_choice] >> return True
-                       -- return False
 
 
 ask_neighbour_room :: IO Room
@@ -153,47 +74,8 @@ ask_neighbour_room = do
                     let next_room = next_room_raw :: Room
                     return next_room
 
-{- 
-go_to_next_room :: Labir -> Room -> My.MyStateT Path (WriterT Log (ReaderT [Room] IO) ) Bool
-go_to_next_room cur_maze next_room = do
-                    cur_next_rooms <- My.lift $ lift ask 
-                    if getCurRoom cur_maze == finish || length cur_next_rooms == 0
-                        then do
-                                My.lift $ tell ["FINISH"]
-                                My.modify (++ [finish])
-                                return True
-                        else if elem next_room cur_next_rooms 
-                                then if getNextRooms cur_maze /= [] 
-                                        then do 
-                                                My.lift $ tell [next_room] 
-                                                My.modify (++ [next_room])
-                                                let next_maze = toNextRoom cur_maze next_room
-                                                let next_rooms2 = getNextRooms next_maze
-                                                liftIO $ putStrLn "Соседние комнаты"
-                                                liftIO $ print next_rooms2
-                                                liftIO $ putStrLn "Введите q, чтобы выйти"
-                                                new_room <- liftIO ask_neighbour_room 
-                                                go_to_next_room next_maze new_room
-                                        else do 
-                                                My.lift $ tell ["next is " ++ next_room]  
-                                                My.modify (next_room :)
-                                                return True
-                                else if next_room /= ("q" :: Room) 
-                                    then do
-                                        My.lift $ tell ["error"] 
-                                        My.modify (("empty room" :: Room) :)
-                                        return False
-                                    else do
-                                        My.lift $ tell ["q"]
-                                        My.modify (++ [gameOver])
-                                        return False -}
 
 
-
--- runIdentity $ runReaderT (runWriterT $ My.runMyStateT (go_to_next_room labirint chlb) []) [chlb, msk, spb]
-
--- result <- runReaderT (runWriterT $ My.runMyStateT (go_to_next_room labirint msk) []) [chlb, msk, spb, kzn, kgd, ekb, uud, krd, smr, finish, mkhch, prm, ufa, grz, rst]
-                    
 go_to_next_room :: Labir -> My.MyStateT [StatE] (WriterT Log (ReaderT [Room] IO) ) Bool
 go_to_next_room cur_maze = do
                 access_rooms <- My.lift $ lift ask
@@ -214,6 +96,7 @@ go_to_next_room cur_maze = do
                                         if next_room == ("look" :: Room) 
                                             then do
                                                 liftIO $ print next_rooms
+                                                My.lift $ tell ["look"]
                                                 liftIO $ putStrLn "Введите название следующей комнаты или 'q' для выхода"
                                                 next_room <- liftIO ask_neighbour_room
                                                 My.lift $ tell [next_room]
@@ -244,18 +127,5 @@ go_to_next_room cur_maze = do
                                         return False
                                             
 
-
-
-                        
-                    
-
-
-
-{- go_to_children :: [Labir] ->  -}
-
-{- exploreChildren :: [Labir] -> RWS.RWS Labir Log Path Bool
-exploreChildren [] = return False
-exploreChildren (c:cs) =
-  explore c >>= \found ->
-  if found then return True
-  else exploreChildren cs -}
+-- новая функция
+-- result <- runReaderT (runWriterT $ My.runMyStateT (go_to_next_room labirint) []) [start, chlb, msk, spb, kzn, kgd, ekb, uud, krd, smr, finish, mkhch, prm, ufa, grz, rst]
--- practice_04/transformer/src/MonadMyStateT.hs
+++ practice_04/transformer/src/MonadMyStateT.hs
@@ -8,27 +8,6 @@ import Control.Monad.IO.Class (MonadIO(..))
 newtype MyStateT s m a = MyStateT { runMyStateT :: s -> m (a, s) }
 
 
-{- -- Функтор
-instance Functor (Writer w) where
-    fmap :: (a -> b) -> Writer w a -> Writer w b
-    fmap f = Writer . update . runWriter
-        where update ~(y, log) = (f y, log)
-{-                   🠕
-            неопровержимый образец
-    (сопоставление происходит только тогда если 
-        вычисления нужны в правой части вычисления.
-        Т.е. это ленивая версия Writer, если убрать ~ то будет строгая версия)
--}
-
-instance Functor m => Functor (WriterT w m) where
-    fmap :: (a -> b) -> WriterT w m a -> WriterT w m b
-    fmap f = WriterT . fmap update . runWriterT
-        where update ~(y, log) = (f y, log)
- -}
-
-{-  instance Functor m => Functor (ReaderT r m) where
-    fmap :: (a -> b) -> ReaderT r m a -> ReaderT r m b
-    fmap f x = ReaderT $ fmap f . runReaderT x -}
 
 instance Functor m => Functor (MyStateT s m) where
     fmap :: (a -> b) -> MyStateT s m a -> MyStateT s m b
@@ -40,43 +19,6 @@ instance Functor m => Functor (MyStateT s m) where
     fmap f x = MyStateT $ fmap update . runMyStateT x 
         where update ~(y, st) = (f y, st)
 
-
-
-{- 
-Аппликативный функтор
-instance (Monoid w, Applicative m) => Applicative (WriterT w m) where
-    pure :: a -> WriterT w m a
-    pure x = WriterT $ pure (x, mempty)
-    (<*>) :: WriterT w m (a -> b) -> WriterT w m a -> WriterT w m b
-    f <*> v = WriterT $ liftA2 update (runWriterT f) (runWriterT v)
-        where update ~(g,w) ~(x,w') = (g x, w `mappend` w') 
-        -}
-
-
-{- instance Applicative m => Applicative (ReaderT r m) where
-    pure :: a -> ReaderT r m a
-    pure = ReaderT . const . pure
---                            🠕
---      нужно поднять оборачиваемое значение в контекст монады
-    (<*>) :: ReaderT r m (a -> b) -> ReaderT r m a -> ReaderT r m b
-    f <*> v = ReaderT $ \ e -> runReaderT f e <*> runReaderT v e
-{-                         :t runReaderT f e  :t runReaderT v e -}
-
-instance Monoid w => Applicative (Writer w) where
-    pure :: a -> Writer w a
-    pure x = Writer (x, mempty)
-    (<*>) :: Writer w (a -> b) -> Writer w a -> Writer w b
-    f <*> v = Writer $ update (runWriter f) (runWriter v)
-        where update ~(g,w) ~(x,w') = (g x, w `mappend` w')
-
-instance (Monoid w, Applicative m) => Applicative (WriterT w m) where
-    pure :: a -> WriterT w m a
-    pure x = WriterT $ pure (x, mempty)
-    (<*>) :: WriterT w m (a -> b) -> WriterT w m a -> WriterT w m b
-    f <*> v = WriterT $ liftA2 update (runWriterT f) (runWriterT v)
-        where update ~(g,w) ~(x,w') = (g x, w `mappend` w')
-
--}
 
 
 {- instance Applicative m => Applicative (MyStateT s m) where
@@ -100,20 +42,6 @@ instance (Applicative m, Monad m) => Applicative (MyStateT s m) where
                                     ~(y, s'') <- runMyStateT x s'
                                     return (g y, s')
 
-    
-
-
-
-{- instance Monad m => Monad (ReaderT r m) where
-    (>>=) :: ReaderT r m a -> (a -> ReaderT r m b) -> ReaderT r m b
-    m >>= k = ReaderT $ \e -> do 
---                            🠕
---              вычисление во внутренней монаде
-        v <- runReaderT m e
-        runReaderT (k v) e
--- TrRW.runReaderT (do {x <- listRead; return ((+1) x)}) 4
-
- -}
 
 instance Monad m => Monad (MyStateT s m) where
 --  (>>=) :: (MyState s m a) -> (\a -> MyState s m b) -> MyState s m b  
@@ -126,10 +54,6 @@ instance Monad m => Monad (MyStateT s m) where
 
 {- Реализация функции fail
 Протаскивание ошибки без дополнительной обработки
-
-instance (Monoid w, MonadFail m) => MonadFail (WriterT w m) where
-    fail :: String -> WriterT w m a
-    fail = WriterT . fail
  -}
 
 
@@ -139,19 +63,6 @@ instance (Monoid w, MonadFail m) => MonadFail (WriterT w m) where
  не обрабатывает ошибки => не нужно -}
 
 
-
-{- Протаскивание внутренней монады наружу
-instance (Monoid w) => MonadTrans (WriterT w) where
-    lift :: Monad m => m a -> WriterT w m a
-    lift m = WriterT $ do
-        x <- m
-        return (x, mempty)
-
-listWrite = WriterT $ [(1,"one"),(2,"two"),(3,"three")]
-{-
-TrRW.runWriterT $ do {x <- listWrite; f <- TrRW.lift [(+4),(*5)]; return (f x)} -}
-
--}
 
 class MonadTrans t where -- Control.Monad.Trans.Class
     lift :: Monad m => m a -> t m a
