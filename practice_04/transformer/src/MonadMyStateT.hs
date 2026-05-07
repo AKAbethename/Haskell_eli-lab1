7f85b5d3540c591c0f5eba43dab257980a30842e
AKAbethename <crazybobov5@gmail.com>
2026-05-07 17:48:27 +0300

    practice_04-ver-2026-05-07-17-49

--- practice_04/transformer/src/MonadMyStateT.hs
+++ practice_04/transformer/src/MonadMyStateT.hs
@@ -1,6 +1,105 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module MonadMyStateT where
 
 newtype MyStateT s m a = MyStateT { runMyStateT :: s -> m (a, s) }
+
+
+{- -- Функтор
+instance Functor (Writer w) where
+    fmap :: (a -> b) -> Writer w a -> Writer w b
+    fmap f = Writer . update . runWriter
+        where update ~(y, log) = (f y, log)
+{-                   🠕
+            неопровержимый образец
+    (сопоставление происходит только тогда если 
+        вычисления нужны в правой части вычисления.
+        Т.е. это ленивая версия Writer, если убрать ~ то будет строгая версия)
+-}
+
+instance Functor m => Functor (WriterT w m) where
+    fmap :: (a -> b) -> WriterT w m a -> WriterT w m b
+    fmap f = WriterT . fmap update . runWriterT
+        where update ~(y, log) = (f y, log)
+ -}
+
+{-  instance Functor m => Functor (ReaderT r m) where
+    fmap :: (a -> b) -> ReaderT r m a -> ReaderT r m b
+    fmap f x = ReaderT $ fmap f . runReaderT x -}
+
+instance Functor m => Functor (MyStateT s m) where
+    fmap :: (a -> b) -> MyStateT s m a -> MyStateT s m b
+--                 f                 x             
+    {- fmap f x = MyStateT $ \st -> do
+                                    (v, st') <- runMyStateT x st 
+                                    return (f v, st') -}
+
+    fmap f x = MyStateT $ fmap update . runMyStateT x 
+        where update ~(y, st) = (f y, st)
+
+
+
+{- 
+Аппликативный функтор
+instance (Monoid w, Applicative m) => Applicative (WriterT w m) where
+    pure :: a -> WriterT w m a
+    pure x = WriterT $ pure (x, mempty)
+    (<*>) :: WriterT w m (a -> b) -> WriterT w m a -> WriterT w m b
+    f <*> v = WriterT $ liftA2 update (runWriterT f) (runWriterT v)
+        where update ~(g,w) ~(x,w') = (g x, w `mappend` w') 
+        -}
+
+
+{- instance Applicative m => Applicative (ReaderT r m) where
+    pure :: a -> ReaderT r m a
+    pure = ReaderT . const . pure
+--                            🠕
+--      нужно поднять оборачиваемое значение в контекст монады
+    (<*>) :: ReaderT r m (a -> b) -> ReaderT r m a -> ReaderT r m b
+    f <*> v = ReaderT $ \ e -> runReaderT f e <*> runReaderT v e
+{-                         :t runReaderT f e  :t runReaderT v e -}
+
+instance Monoid w => Applicative (Writer w) where
+    pure :: a -> Writer w a
+    pure x = Writer (x, mempty)
+    (<*>) :: Writer w (a -> b) -> Writer w a -> Writer w b
+    f <*> v = Writer $ update (runWriter f) (runWriter v)
+        where update ~(g,w) ~(x,w') = (g x, w `mappend` w')
+
+instance (Monoid w, Applicative m) => Applicative (WriterT w m) where
+    pure :: a -> WriterT w m a
+    pure x = WriterT $ pure (x, mempty)
+    (<*>) :: WriterT w m (a -> b) -> WriterT w m a -> WriterT w m b
+    f <*> v = WriterT $ liftA2 update (runWriterT f) (runWriterT v)
+        where update ~(g,w) ~(x,w') = (g x, w `mappend` w')
+
+-}
+
+
+{- instance Applicative m => Applicative (MyStateT s m) where
+    pure :: a -> MyStateT s m a
+    pure x = MyStateT $ \st -> pure (x, st) 
+    (<*>) :: MyStateT s m (a -> b) -> MyStateT s m a -> MyStateT s m b
+--                        f                      x
+--    f <*> x = MyStateT $ \st -> (runMyStateT f st) <*> (runMyStateT x st)
+    f <*> x = MyStateT $ \st -> liftA2 update (runMyStateT f st) (runMyStateT x st)
+                                    where update ~(g, s') ~(y, s'') = (g y, s') -}
+
+
+instance (Applicative m, Monad m) => Applicative (MyStateT s m) where
+    -- pure :: a -> MyStateT s m a
+    pure x = MyStateT $ \st -> return (x, st)
+
+    -- (<*>) :: MyStateT s m (a -> b) -> MyStateT s m a -> MyStateT s m b
+    --                      f                      x 
+    f <*> x = MyStateT $ \st -> do
+                                    ~(g, s') <- runMyStateT f st 
+                                    ~(y, s'') <- runMyStateT x s'
+                                    return (g y, s')
+
+    
+
+
 
 {- instance Monad m => Monad (ReaderT r m) where
     (>>=) :: ReaderT r m a -> (a -> ReaderT r m b) -> ReaderT r m b
@@ -17,7 +116,7 @@ instance Monad m => Monad (MyStateT s m) where
 --  (>>=) :: (MyState s m a) -> (\a -> MyState s m b) -> MyState s m b  
 --                 m                      k
     m >>= k = MyStateT $ \st -> do
-                                    (v, st') <- runMyStateT st  -- (v, st') <=> (a, s)
+                                    (v, st') <- runMyStateT m st  -- (v, st') <=> (a, s)
                                     runMyStateT (k v) st'
 
 
@@ -30,8 +129,11 @@ instance (Monoid w, MonadFail m) => MonadFail (WriterT w m) where
     fail = WriterT . fail
  -}
 
-instance MonadFail m => MonadFail (MyState s m) where
+
+{- instance MonadFail m => MonadFail (MyStateT s m) where
+    fail :: String -> MyStateT s m a
     fail = MyStateT . fail
+ не обрабатывает ошибки => не нужно -}
 
 
 
@@ -47,6 +149,9 @@ listWrite = WriterT $ [(1,"one"),(2,"two"),(3,"three")]
 TrRW.runWriterT $ do {x <- listWrite; f <- TrRW.lift [(+4),(*5)]; return (f x)} -}
 
 -}
+
+class MonadTrans t where -- Control.Monad.Trans.Class
+    lift :: Monad m => m a -> t m a
 
 instance MonadTrans (MyStateT s) where
 --    lift :: Monad m => m a -> MyStateT s m a 
